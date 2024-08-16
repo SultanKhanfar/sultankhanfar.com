79348a958f8ab654bebce093db151ffb8ee1f790
Sultan Khanfar <sultankhanfar@Sultans-MacBook-Air.local>
2024-08-16 09:25:12 -0400

    Initial Commit

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Sultan Khanfar</h2>;
-  const three = <h3 className="big-heading">Student and Scientist</h3>;
+  const three = <h3 className="big-heading">Neuron-Glia Researcher</h3>;
   const four = (
     <>
       <p>
